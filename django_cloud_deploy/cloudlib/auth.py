06a9b78d95cb9510a1b2755088e3c7cbe91b5dab
Xiaofei Wang <6218006+wangxf123456@users.noreply.github.com>
2018-12-03 17:27:33 -0800

    Use "gcloud login" instead of "gcloud application-default login".

--- django_cloud_deploy/cloudlib/auth.py
+++ django_cloud_deploy/cloudlib/auth.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import subprocess
 
 import google.auth
@@ -30,8 +31,40 @@ class AuthClient(object):
         Returns:
             Credentials Object
         """
-        command = ['gcloud', 'auth', 'application-default', 'login', '-q']
+
+        # We can not use "gcloud auth application-default login" here because
+        # we use "gcloud app deploy" to deploy a Django app to app-engine. It
+        # requires "gcloud auth login".
+        command = ['gcloud', 'auth', 'login', '-q']
         subprocess.check_call(
             command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = (
+            self._get_active_account_adc_path())
         creds, _ = google.auth.default()
         return creds
+
+    @staticmethod
+    def _get_active_account_adc_path() -> str:
+        """Get application default credentials path of the given account.
+
+        After we run "gcloud auth login", a credentials file for the login
+        account is created under
+        "~/.config/gcloud/legacy_credentials/<account>/adc.json". The accesses
+        given by "gcloud auth login" is a superset of
+        "gcloud auth application-default login".
+
+        Returns:
+            Absolute path of the application default credentials path of the
+            given account.
+        """
+        command = ['gcloud', 'info',
+                   '--format=value(config.paths.global_config_dir)']
+        gcloud_config_path = subprocess.check_output(
+            command, universal_newlines=True).rstrip()
+        command = ['gcloud', 'info', '--format=value(config.account)']
+        active_account = subprocess.check_output(
+            command, universal_newlines=True).rstrip()
+        # These hardcoded values are also hardcoded by gcloud.
+        return os.path.join(gcloud_config_path, 'legacy_credentials',
+                            active_account, 'adc.json')
+
